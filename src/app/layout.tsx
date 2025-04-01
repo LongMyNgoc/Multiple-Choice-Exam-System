7d2d31b9eec829d3dbc9138c9ed2ccc475d5d536
LongMyNgoc <longmyngoc2004@gmail.com>
2025-04-01 16:32:23 +0700

    Fixed Error Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,9 +10,9 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
+}); 
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Hệ thống thi trắc nghiệm",
   description: "Ứng dụng tạo câu hỏi trắc nghiệm, tổ chức thi và quản lý kết quả thi. Cung cấp nhiều loại câu hỏi cho người dùng với các tính năng quản lý dễ dàng.",
   keywords: "trắc nghiệm, thi trực tuyến, quản lý câu hỏi, hệ thống thi, ứng dụng thi, multiple choice, quản lý kết quả thi",
