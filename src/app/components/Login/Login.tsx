4d8442f4c3a255ff6d30e9b4f9090636604a5cd3
LongMyNgoc <longmyngoc2004@gmail.com>
2025-04-01 22:15:43 +0700

    Fixed Error Vercel Type any

--- src/app/components/Login/Login.tsx
+++ src/app/components/Login/Login.tsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 import Login_Image from "../../assets/Login/Login_Image.png";
 import { FcGoogle } from "react-icons/fc";
 
-const hoverEffect = (e: any, type: "in" | "out") => {
+const hoverEffect = (e: React.MouseEvent<HTMLButtonElement>, type: "in" | "out") => {
   e.currentTarget.style.filter = type === "in" ? "brightness(1.1)" : "brightness(1)";
 };
-const clickEffect = (e: any, type: "down" | "up") => {
+const clickEffect = (e: React.MouseEvent<HTMLButtonElement>, type: "down" | "up") => {
   e.currentTarget.style.transform = type === "down" ? "scale(0.95)" : "scale(1)";
 };
-const linkHover = (e: any, type: "in" | "out") => {
-  e.currentTarget.style.color = type === "in" ? "#FF4081" : "#007BFF"; 
+const linkHover = (e: React.MouseEvent<HTMLAnchorElement>, type: "in" | "out") => {
+  e.currentTarget.style.color = type === "in" ? "#FF4081" : "#007BFF";
   e.currentTarget.style.transform = type === "in" ? "scale(1.1)" : "scale(1)";
 };
 
