7a5b7c0fe71b97924ccddfb74874d3cb9439a1d3
LongMyNgoc <longmyngoc2004@gmail.com>
2025-04-02 15:57:32 +0700

    Fixed Error Vercel Type Any

--- src/app/utils/auth.ts
+++ src/app/utils/auth.ts
@@ -1,6 +1,7 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../lib/firebase";
 import { toast } from "react-toastify";
+import { FirebaseError } from "firebase/app";
 
 /**
  * Đăng nhập người dùng với email và password
@@ -14,29 +15,31 @@ export const loginWithEmail = async (email: string, password: string) => {
     toast.success("Đăng nhập thành công!");
     localStorage.setItem("userEmail", email);
     return { user: userCredential.user, error: null };
-  } catch (error: any) {
+  } catch (error: unknown) {
     let errorMessage = "Có lỗi xảy ra. Vui lòng thử lại.";
 
-    // Kiểm tra mã lỗi từ Firebase và đặt thông báo thân thiện
-    switch (error.code) {
-      case "auth/user-not-found":
-        errorMessage = "Không tìm thấy tài khoản. Vui lòng kiểm tra lại email.";
-        break;
-      case "auth/wrong-password":
-        errorMessage = "Mật khẩu không đúng. Vui lòng nhập lại.";
-        break;
-      case "auth/invalid-email":
-        errorMessage = "Địa chỉ email không hợp lệ. Hãy kiểm tra lại.";
-        break;
-      case "auth/user-disabled":
-        errorMessage = "Tài khoản của bạn đã bị vô hiệu hóa. Hãy liên hệ hỗ trợ.";
-        break;
-      case "auth/invalid-credential":
-        errorMessage = "Email hoặc mật khẩu không chính xác. Vui lòng thử lại.";
-        break;
-      default:
-        errorMessage = "Đăng nhập thất bại. Hãy thử lại sau.";
-        break;
+    if (error instanceof FirebaseError) {
+      // Kiểm tra mã lỗi từ Firebase và đặt thông báo thân thiện
+      switch (error.code) {
+        case "auth/user-not-found":
+          errorMessage = "Không tìm thấy tài khoản. Vui lòng kiểm tra lại email.";
+          break;
+        case "auth/wrong-password":
+          errorMessage = "Mật khẩu không đúng. Vui lòng nhập lại.";
+          break;
+        case "auth/invalid-email":
+          errorMessage = "Địa chỉ email không hợp lệ. Hãy kiểm tra lại.";
+          break;
+        case "auth/user-disabled":
+          errorMessage = "Tài khoản của bạn đã bị vô hiệu hóa. Hãy liên hệ hỗ trợ.";
+          break;
+        case "auth/invalid-credential":
+          errorMessage = "Email hoặc mật khẩu không chính xác. Vui lòng thử lại.";
+          break;
+        default:
+          errorMessage = "Đăng nhập thất bại. Hãy thử lại sau.";
+          break;
+      }
     }
 
     // Hiển thị thông báo lỗi dễ hiểu
