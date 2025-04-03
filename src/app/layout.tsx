b927c055dabeadd4ff77330e00a1a44bd9d3f989
LongMyNgoc <longmyngoc2004@gmail.com>
2025-04-03 23:17:18 +0700

    Rename Title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 }); 
 
 export const metadata: Metadata = {
-  title: "Hệ thống thi trắc nghiệm",
+  title: "Multiple Choice Exam System",
   description: "Ứng dụng tạo câu hỏi trắc nghiệm, tổ chức thi và quản lý kết quả thi. Cung cấp nhiều loại câu hỏi cho người dùng với các tính năng quản lý dễ dàng.",
   keywords: "trắc nghiệm, thi trực tuyến, quản lý câu hỏi, hệ thống thi, ứng dụng thi, multiple choice, quản lý kết quả thi",
 };
